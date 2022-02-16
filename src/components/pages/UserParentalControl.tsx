49488781b228eba1f73b514a398c4ec62b0b3db8
Dmitry Lyzo <ashephard0@gmail.com>
2022-02-16 19:16:31 +0300

    Fix missing property TypeScript error

--- src/components/pages/UserParentalControl.tsx
+++ src/components/pages/UserParentalControl.tsx
@@ -397,6 +397,7 @@ const UserParentalControl: FunctionComponent = () => {
                                 return <AccessScheduleList
                                     key={index}
                                     index={index}
+                                    Id={accessSchedule.Id}
                                     DayOfWeek={accessSchedule.DayOfWeek}
                                     StartHour={accessSchedule.StartHour}
                                     EndHour={accessSchedule.EndHour}
