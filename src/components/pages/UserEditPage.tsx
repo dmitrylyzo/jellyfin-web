fd0ee9793949799b98755bd6c811bb09a1e38c6c
Dmitry Lyzo <ashephard0@gmail.com>
2022-02-18 21:36:05 +0300

    [hack] Fix type mismatch TypeScript error

--- src/components/pages/UserEditPage.tsx
+++ src/components/pages/UserEditPage.tsx
@@ -239,7 +239,7 @@ const UserEditPage: FunctionComponent = () => {
                 user.Policy.SyncPlayAccess = (elem.querySelector('#selectSyncPlayAccess') as HTMLInputElement).value as SyncPlayUserAccessType;
             }
             window.ApiClient.updateUser(user).then(function () {
-                window.ApiClient.updateUserPolicy(user.Id, user.Policy).then(function () {
+                window.ApiClient.updateUserPolicy(user.Id || '', user.Policy || {}).then(function () {
                     onSaveComplete();
                 });
             });
