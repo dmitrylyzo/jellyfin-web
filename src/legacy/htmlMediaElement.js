176fd6804472c812d37ce6c8cbe408815a73d129
Dmitry Lyzo <ashephard0@gmail.com>
2022-03-29 23:34:58 +0300

    fix: Don't polyfill good HTMLMediaElement.play

--- src/legacy/htmlMediaElement.js
+++ src/legacy/htmlMediaElement.js
@@ -7,20 +7,42 @@
 (function (HTMLMediaElement) {
     'use strict';
 
-    const HTMLMediaElement_proto = HTMLMediaElement.prototype;
-    const real_play = HTMLMediaElement_proto.play;
+    function isPlayPromised() {
+        const elem = document.createElement('audio');
+        elem.classList.add('testMediaPlayerAudio');
+        elem.classList.add('hide');
 
-    HTMLMediaElement_proto.play = function () {
+        document.body.appendChild(elem);
+
+        elem.volume = 1; // Volume should not be zero to trigger proper permissions
+        elem.src = 'assets/audio/silence.mp3'; // Silent sound
+
+        let promised = false;
+
         try {
-            const promise = real_play.apply(this, arguments);
-
-            if (typeof promise?.then === 'function') {
-                return promise;
-            }
-
-            return Promise.resolve();
+            const promise = elem.play();
+            promised = typeof promise?.then === 'function';
         } catch (err) {
-            return Promise.reject(err);
+            console.error('HTMLMediaElement.play test failed', err);
         }
-    };
+
+        elem.pause();
+        elem.remove();
+
+        return promised;
+    }
+
+    if (!isPlayPromised()) {
+        const HTMLMediaElement_proto = HTMLMediaElement.prototype;
+        const real_play = HTMLMediaElement_proto.play;
+
+        HTMLMediaElement_proto.play = function () {
+            try {
+                real_play.apply(this, arguments);
+                return Promise.resolve();
+            } catch (err) {
+                return Promise.reject(err);
+            }
+        };
+    }
 }(HTMLMediaElement));
